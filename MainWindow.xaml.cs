595c15a6f5e05ad69044d9b267e7e2e22b9c776f
yakira <ynwint@student.rtc.edu>
2023-02-17 23:31:29 -0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace Midterm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Product> products = new List<Product>();
+        private object lbProducts;
+        private int i;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Preload();
+            DisplayInformation();
+            DisplayInformation();
+            DisplayInformation();
+        }
+
+        void Preload()
+
+        {
+            Random rand = new Random();
+            int end = 106;
+
+
+
+            for (int i = 100; i < end; i++)
+            {
+                string productName = "Red Bull: " + i;
+
+                if (rand.Next(2) != 0)
+                {
+                    products.Add(new Product(productName));
+                }
+                else
+                {
+                    products.Add(new Product(productName, false));
+                }
+            }
+        }
+        public void DisplayInformation()
+        {
+
+            for (int i = 0; i <products.Count; i++)
+            {
+                lbProducts.items.Add(products[i]);
+            }
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    internal class Product
+    {
+        string _name;
+        string _sku;
+        private bool _instock;
+        bool _inStock;
+
+        public Product(string name, bool inStock = true)
+        {
+            Random rand = new Random();
+
+            _name = name;
+            _sku = rand.Next(1000000, 10000000).ToString();
+            _inStock = inStock;
+        }
+       
+        // overloading
+        public Product(string name, string sku)
+        {
+            _name = name;
+            _sku = sku;
+            _inStock = true;
+        }
+        
+        // Constructors don't have return types
+        public Product(string name, string sku, bool inStock)
+        {
+            _name = name;
+            _sku = sku;
+            _instock = inStock;
+        }
+
+        public string Name { get => _name;}
+        public string Sku { get => _sku; set => _sku = value; }
+        public bool InStock { get => _inStock; set => _inStock = value; }
+
+        // Override to string, to change what display when you call ToString() on an object
+        public override string ToString()
+        {
+            // If in stock, display name and sku 
+            // else display name and out of stock
+            if(InStock)
+            {
+                return $"{_name} - Sku: {_sku}";
+            }    
+            else
+            {
+                return $"{_name} - Out Of Stock";
+            }
+        }
+    }
+}
